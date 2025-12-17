f9bf3c75acaaa521c393901c21b88537d56a47e4
amar <amarnadh.t@hotmail.com>
2025-12-17 11:04:38 +0530

    initial docker files

---
+++ code/main.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, Docker!</h1>'
+
+if __name__ == "__main__":
+    # The application runs on port 8000 inside the container
+    app.run(host="0.0.0.0", port=8000)
