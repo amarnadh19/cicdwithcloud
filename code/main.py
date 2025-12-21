90de968303f9179fd5db540887a5fe6111ad7a82
lavanya <lavanyas534@gmail.com>
2025-12-21 11:39:11 +0530

    helm update lavanya -13

--- code/main.py
+++ code/main.py
@@ -10,4 +10,4 @@ def hello():
 
 if __name__ == "__main__":
     # The application runs on port 8000 inside the container
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8080)
